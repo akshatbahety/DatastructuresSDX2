16a1184e33ee2c98d1c25fc51f95a924630fe9c6
Akshat Bahety <akicool100@gmail.com>
2018-01-16 06:24:20 +0530

    Completed with all test passing for these methods.

--- src/MovieRatingsProcessor.java
+++ src/MovieRatingsProcessor.java
@@ -4,23 +4,53 @@
  * Please be sure not to change the method signatures!
  */
 
-import java.util.List;
-import java.util.PriorityQueue;
-import java.util.TreeMap;
+import sun.reflect.generics.tree.Tree;
+
+import java.util.*;
 
 
 public class MovieRatingsProcessor {
 
 	public static List<String> getAlphabeticalMovies(TreeMap<String, PriorityQueue<Integer>> movieRatings) {
 		
-		/* IMPLEMENT THIS METHOD! */
-		
-		return null; // this line is here only so this code will compile if you don't modify it
+		/* IMPLEMENT THIS METHOD! *
+		 */
+        ArrayList<String> list = new ArrayList<>();
+
+        if (movieRatings == null || movieRatings.isEmpty())
+            return list;
+
+        Set<String> temp = movieRatings.keySet();
+        for (String a : temp)
+        {
+            list.add(a);
+        }
+
+
+        if(list.size()> 0)
+            return list;
+
+        return null; // this line is here only so this code will compile if you don't modify it
 	}
 
 	public static List<String> getAlphabeticalMoviesAboveRating(TreeMap<String, PriorityQueue<Integer>> movieRatings, int rating) {
 		
 		/* IMPLEMENT THIS METHOD! */
+        ArrayList<String> list = new ArrayList<>();
+
+        if (movieRatings == null || movieRatings.isEmpty())
+            return list;
+
+        Set<String> set = movieRatings.keySet();
+
+        for(String s : set) {
+                PriorityQueue<Integer> pq = movieRatings.get(s);
+            if(pq.peek() > rating)
+                list.add(s);
+        }
+
+        if(list.size() > 0 || list.isEmpty())
+         return  list;
 		
 		return null; // this line is here only so this code will compile if you don't modify it
 	}
@@ -28,7 +58,49 @@ public class MovieRatingsProcessor {
 	public static TreeMap<String, Integer> removeAllRatingsBelow(TreeMap<String, PriorityQueue<Integer>> movieRatings, int rating) {
 		
 		/* IMPLEMENT THIS METHOD! */
-		
-		return null; // this line is here only so this code will compile if you don't modify it
+
+		TreeMap<String , Integer> tm = new TreeMap<>();
+		ArrayList<String> templ = new ArrayList<>();
+        if (movieRatings == null || movieRatings.isEmpty())
+            return tm;
+
+        Set<String> set = movieRatings.keySet();
+
+        for(String s : set)
+        {
+
+            PriorityQueue<Integer> pq = movieRatings.get(s);
+            List<Integer> list = new ArrayList<>(pq);
+            int temp = pq.size();
+            int counter = 0;
+
+            for (int a : list) {
+
+                if(a < rating) {
+                    pq.remove(a);
+                    counter++;
+                }
+            }
+
+            if(counter == temp) {
+                templ.add(s);
+            }
+
+            if(counter > 0)
+                tm.put(s,counter);
+
+        }
+
+        for(String s : templ)
+        {
+            movieRatings.remove(s);
+        }
+
+
+        if(tm.size() > 0 || tm.isEmpty())
+            return tm;
+
+
+        return null; // this line is here only so this code will compile if you don't modify it
 	}
 }
